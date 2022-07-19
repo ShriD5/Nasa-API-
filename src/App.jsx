3020140597e929184651acf07bef645d809d2ede
Shrithan <shrithanofficial@gmail.com>
2022-07-19 15:45:56 +0530

    Update/axios-req

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,38 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import './App.css'
+import React, { useState, useEffect } from "react"
+import axios from "./axios"
 
-function App() {
-  const [count, setCount] = useState(0)
+const App = () => {
+  const [apod, setApod] = useState({})
+
+  useEffect(() => {
+    axios.getApod().then(apodData => {
+      setApod(apodData.data)
+    })
+  }, [])
 
   return (
-    <div className="App">
-    Nice
+    <div style={{ maxWidth: 900, padding: 30 }}>
+      <h1>NASA API</h1>
+      <h2>Astronomy Picture of the Day</h2>
+      {apod && (
+        <article>
+          <header>
+            {apod.title} - <i>{apod.date}</i>
+          </header>
+          <img src={apod.url} alt="APOD" width="870" height="auto" />
+          <p>{apod.explanation}</p>
+          <pre
+            style={{
+              overflowX: "auto",
+              whiteSpace: "pre-wrap",
+              wordWrap: "break-word",
+            }}
+          >
+            <hr />
+            {JSON.stringify(apod, null, 2)}
+          </pre>
+        </article>
+      )}
     </div>
   )
 }
